86dcb51e9ba6796ad05a183d736f0eeae1483ba0
kuldeep <mail.kuldeepvish@gmail.com>
2025-06-19 03:34:48 +0530

    Modiefied Data

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,31 @@ import React from "react";
 const token = import.meta.env.TOKEN;
 const App = () => {
   const [body, setBody] = React.useState({
-    endDate: "2025-06-09T11:14:18.505Z",
+    startDate:"2025-06-19T11:14:18.505Z",
+    endDate: "2025-06-20T11:14:18.505Z",
+    name: "Astro Vahlay Meeting",
     isLocked: true,
     roomMode: "group",
-    roomNamePrefix: "example-prefix",
+    roomNamePrefix: "astro-vahlay",
     roomNamePattern: "uuid",
     templateType: "viewerMode",
     fields: ["hostRoomUrl"],
+    
   });
   const [response, setResponse] = React.useState(null);
   const createMeeting = async () => {
     try {
-      const res = await fetch("https://where-by-backend.onrender.com/api/create-room", {
+      const res = await fetch("https://cloudastro.space/api/create-room", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
+          "Authorization": `Bearer ${token}`,
         },
         body: JSON.stringify(body),
       });
       const data = await res.json();
-      setResponse(data);
-      console.log("Meeting created:", data);
+      setResponse(data.meeting);
+      console.log("Meeting created:", data?.meeting);
     } catch (error) {
       console.error("Error creating meeting:", error);
     }
@@ -45,7 +48,7 @@ const App = () => {
       {response && (
         <div>
           <h2>Meeting Created</h2>
-          <p>Meeting Start Date: {response?.startDate ?? "NO RECORD"}</p>
+          <p>Meeting Start Date: {JSON.stringify(response)}</p>
           <p>Meeting End Date:{response?.endDate ?? "NO RECORD"}</p>
           <p>Meeting ID: {response?.meetingId ?? "NO RECORD"}</p>
           <p>Meeting Room Name: {response?.roomName ?? "NO RECORD"}</p>
